4efd436d6dd0ac5f523fb3de471bc8aba626bf70
Paolo Denti <paolo.denti@gmail.com>
2023-12-25 21:25:34 -0800

    tests

--- src/test/java/com/github/paolodenti/grpctestserver/GrpcTestServerApplicationTests.java
+++ src/test/java/com/github/paolodenti/grpctestserver/GrpcTestServerApplicationTests.java
@@ -1,13 +1,26 @@
 package com.github.paolodenti.grpctestserver;
 
+import com.github.paolodenti.grpctestproto.HelloWorldRequest;
+import com.github.paolodenti.grpctestproto.HelloWorldServiceGrpc;
+import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class GrpcTestServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @GrpcClient("grpc-server")
+    private HelloWorldServiceGrpc.HelloWorldServiceBlockingStub helloWorldServiceBlockingStub;
 
+    @Test
+    void testServer() {
+
+        var request = HelloWorldRequest.newBuilder()
+                .setRequestMessage("Hello World from test")
+                .build();
+        var response = helloWorldServiceBlockingStub.helloWorld(request);
+        assertTrue(response.getResponseMessage().startsWith("Replied to 'Hello World from test'"));
+    }
 }
